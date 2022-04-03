adc9b6761cfa738bd1798fe1b7c10b257cccb40e
Akshata Naik <37590188+akshata05@users.noreply.github.com>
2022-04-03 23:48:14 +0530

    First

---
+++ src/main/java/com/example/AWS5/Aws5Application.java
@@ -0,0 +1,13 @@
+package com.example.AWS5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "controller")
+public class Aws5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aws5Application.class, args);
+	}
+
+}
