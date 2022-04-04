3fc4375dbc60b58679d5c6538a5a900093778b8f
Akshata Naik <37590188+akshata05@users.noreply.github.com>
2022-04-04 13:10:56 +0530

    Second

--- src/main/java/controller/First.java
+++ src/main/java/controller/First.java
@@ -3,11 +3,14 @@ package controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class First {
 @RequestMapping("/")
-public String Greeting() {
-	return "Hello World!!";
+public ModelAndView Greeting() {
+	 ModelAndView modelAndView = new ModelAndView();
+	    modelAndView.setViewName("index.html");
+	    return modelAndView;
 }
 }
